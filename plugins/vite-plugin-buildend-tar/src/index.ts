1c6dc9056343dcb8dda95488492cea951a266bb5
ryanYan <vue12306@163.com>
2025-07-23 16:04:59 +0800

    :bug: fix: fix

--- plugins/vite-plugin-buildend-tar/src/index.ts
+++ plugins/vite-plugin-buildend-tar/src/index.ts
@@ -3,6 +3,7 @@ import path from "path";
 import archiver from "archiver";
 import { createGzip } from "zlib";
 import OSS from "ali-oss";
+import dayjs from "dayjs";
 
 const uploadToOSS = async (fileName: string, filePath: string) => {
   const client = new OSS({
@@ -19,16 +20,6 @@ const uploadToOSS = async (fileName: string, filePath: string) => {
     throw err;
   }
 };
-
-const formatTime = (date: Date) => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  return `${year}${month}${day}${hours}${minutes}`;
-};
-
 const buildEndTar = () => {
   let webStaticFilePath: string;
   let electronStaticFilePath: string;
@@ -55,7 +46,7 @@ const buildEndTar = () => {
         const zipFilePath = path.resolve(
           process.cwd(),
           "dist",
-          `PMG_V${packageJson.version}-${formatTime(new Date())}.tar.gz`
+          `PMG_V${packageJson.version}-${dayjs().format("YYYYMMDDHHmm")}.tar.gz`
         );
         const output = fs.createWriteStream(zipFilePath);
         const archive = archiver("tar", {
