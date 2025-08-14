ac30a18ca6d1e38e51c7f5c76fc27e9b6719b5a7
ryanYan <vue12306@163.com>
2025-08-14 09:42:04 +0800

    :bug: fix: 优化buildendZipped打包目录划分

--- plugins/vite-plugin-buildend-zipped/src/buildendZipped.js
+++ plugins/vite-plugin-buildend-zipped/src/buildendZipped.js
@@ -1,115 +1,100 @@
-import fs from "fs";
-import path from "path";
-import archiver from "archiver";
-import { createGzip } from "zlib";
-import OSS from "ali-oss";
+import fs from "fs"
+import path from "path"
+import archiver from "archiver"
+import { createGzip } from "zlib"
+import OSS from "ali-oss"
 
 const uploadToOSS = (ossSavePath, originPaths = []) => {
   return new Promise(async (resolve, reject) => {
-    const accessKeyId = process.env.OSS_ACCESS_KEY_ID || "";
-    const accessKeySecret = process.env.OSS_ACCESS_KEY_SECRET || "";
+    const accessKeyId = process.env.OSS_ACCESS_KEY_ID || ""
+    const accessKeySecret = process.env.OSS_ACCESS_KEY_SECRET || ""
     if (!accessKeyId || !accessKeySecret) {
-      console.log("🚨 未读取到OSS_ACCESS_KEY_ID或OSS_ACCESS_KEY_SECRET配置");
-      reject();
+      console.log("🚨 未读取到OSS_ACCESS_KEY_ID或OSS_ACCESS_KEY_SECRET配置")
+      reject()
     }
     const client = new OSS({
       region: "oss-cn-shanghai",
       accessKeyId,
       accessKeySecret,
       bucket: "jg-deliver",
-      authorizationV4: true,
-    });
+      authorizationV4: true
+    })
     try {
-      if (!originPaths.length)
-        return console.log("🚨 无上传文件，请确认文件是否存在");
+      if (!originPaths.length) return console.log("🚨 无上传文件，请确认文件是否存在")
       for (const i of originPaths) {
-        const targetFileName = `${ossSavePath}${path.basename(i)}`;
+        const targetFileName = `${ossSavePath}${path.basename(i)}`
         try {
-          await client.head(targetFileName);
-          await client.delete(targetFileName);
+          await client.head(targetFileName)
+          await client.delete(targetFileName)
         } catch (err) {
-          if (err.code !== "NoSuchKey") throw err;
+          if (err.code !== "NoSuchKey") throw err
         }
-        const res = await client.put(targetFileName, i);
-        console.log(`📤 文件已成功上传，下载地址：${res.url}`);
+        const res = await client.put(targetFileName, i)
+        console.log(`📤 文件已成功上传，下载地址：${res.url}`)
       }
-      resolve();
+      resolve()
     } catch (err) {
-      console.log("🚨 OSS上传失败", err);
-      reject();
+      console.log("🚨 OSS上传失败", err)
+      reject()
     }
-  });
-};
-export const buildEndZipped = ({
+  })
+}
+export default function buildEndZipped({
   needUpload = true,
-  project_shortname = "pmg",
-  target_oss_object = "jg-web-test/main-server",
-} = {}) => {
-  let webStaticFilePath;
-  let appStaticFilePath;
-  let mode;
+  proShortName = "pmg",
+  targetOssObject = "jg-web-test/main-server"
+} = {}) {
+  let webStaticFilePath
+  let appStaticFilePath
+  let mode
   return {
     name: "vite-plugin-buildend-zipped",
     apply: "build",
     configResolved(viteConfig) {
-      webStaticFilePath = path.resolve(viteConfig.build.outDir);
-      appStaticFilePath = path.resolve(process.cwd(), "dist/appImage");
-      mode = viteConfig.mode;
+      webStaticFilePath = path.resolve(viteConfig.build.outDir)
+      appStaticFilePath = path.resolve(process.cwd(), "dist/appImage")
+      mode = viteConfig.mode
     },
     closeBundle: {
       sequential: true,
       order: "post",
       handler: async () => {
-        if (mode !== "prod") return;
-        if (!project_shortname)
-          return console.log("🚨 请填写项目名称简写，例如：pmg");
-        if (!target_oss_object)
-          return console.log(
-            "🚨 请填写AliOSS存储对象，例如：jg-web-test/main-server"
-          );
-        const packageJsonPath = path.resolve(process.cwd(), "package.json");
-        if (!fs.existsSync(packageJsonPath)) return;
-        const packageJson = JSON.parse(
-          fs.readFileSync(packageJsonPath, "utf-8")
-        );
-        const pkgPath = path.resolve(
-          process.cwd(),
-          "dist",
-          `${packageJson.version}`
-        );
-        const webPath = path.resolve(
-          pkgPath,
-          `${project_shortname}-frontend-${packageJson.version}.tar.gz`
-        );
-        const appPath = path.resolve(
-          pkgPath,
-          `${project_shortname}-app-${packageJson.version}.tar.gz`
-        );
-        if (fs.existsSync(pkgPath))
-          fs.rmSync(pkgPath, { recursive: true, force: true });
-        fs.mkdirSync(pkgPath, { recursive: true });
-        console.log(`📁 成功创建产物目录：${pkgPath}`);
-        console.log(`🚚 开始构建产物压缩包`);
-        for (const item of [webPath, appPath]) {
-          const archive = archiver("tar", { zlib: { level: 9 } });
-          const gzip = createGzip();
-          const output = fs.createWriteStream(item);
-          archive.pipe(gzip).pipe(output);
+        if (mode !== "prod") return
+        if (!proShortName) return console.log("🚨 请填写项目名称简写，例如：pmg")
+        if (!targetOssObject) return console.log("🚨 请填写AliOSS存储对象，例如：jg-web-test/main-server")
+        const packageJsonPath = path.resolve(process.cwd(), "package.json")
+        if (!fs.existsSync(packageJsonPath)) return
+        const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"))
+        const appX86PathBaseName = `${proShortName}-app-${packageJson.version}-x86_64`
+        const appArmPathBaseName = `${proShortName}-app-${packageJson.version}-arm64`
+        const pkgPath = path.resolve(process.cwd(), "dist", `${packageJson.version}`)
+        const webPath = path.resolve(pkgPath, `${proShortName}-frontend-${packageJson.version}.tar.gz`)
+        const appX86Path = path.resolve(pkgPath, `${appX86PathBaseName}.tar.gz`)
+        const appArmPath = path.resolve(pkgPath, `${appArmPathBaseName}.tar.gz`)
+        if (fs.existsSync(pkgPath)) fs.rmSync(pkgPath, { recursive: true, force: true })
+        fs.mkdirSync(pkgPath, { recursive: true })
+        console.log(`📁 成功创建产物目录：${pkgPath}`)
+        console.log(`🚚 开始构建产物压缩包`)
+        for (const item of [webPath, appX86Path, appArmPath]) {
+          const archive = archiver("tar", { zlib: { level: 9 } })
+          const gzip = createGzip()
+          const output = fs.createWriteStream(item)
+          archive.pipe(gzip).pipe(output)
           if (item === webPath && fs.existsSync(webStaticFilePath)) {
-            archive.directory(webStaticFilePath, false);
-          } else if (item === appPath && fs.existsSync(appStaticFilePath)) {
-            archive.directory(appStaticFilePath, false);
+            archive.directory(webStaticFilePath, false)
+          } else if (item === appX86Path && fs.existsSync(appStaticFilePath)) {
+            archive.glob("**/*x86_64*", { cwd: appStaticFilePath }, { prefix: appX86PathBaseName })
+          } else if (item === appArmPath && fs.existsSync(appStaticFilePath)) {
+            archive.glob("**/*arm64*", { cwd: appStaticFilePath }, { prefix: appArmPathBaseName })
           }
-          await archive.finalize();
+          await archive.finalize()
         }
-        console.log(
-          `👽️ 压缩包构建完成${needUpload ? "，准备上传阿里云OSS" : ""}`
-        );
-        if (!needUpload) return;
-        const ossSavePath = `${target_oss_object}/${packageJson.version}/`;
-        await uploadToOSS(ossSavePath, [webPath, appPath]);
-        console.log(`🚀 文件已全部上传`);
-      },
-    },
-  };
-};
+        console.log(`👽️ 压缩包构建完成${needUpload ? "，准备上传阿里云OSS" : ""}`)
+        if (!needUpload) return
+        const ossSavePath = `${targetOssObject}/${packageJson.version}/`
+        await uploadToOSS(ossSavePath, [webPath, appX86Path, appArmPath])
+        console.log(`🚀 文件已全部上传`)
+      }
+    }
+  }
+}
